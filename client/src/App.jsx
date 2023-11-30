e6337d3f8da08c4a65cd7068e8c32c4abe713572
Arghya Maity <maityarghya0@gmail.com>
2023-12-01 01:26:31 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@material-tailwind/react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl">Hello world!</h1>
+      <Button>Button</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-undef
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+export default withMT({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+})
+
